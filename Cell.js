eaf6cad5b4858da94b019cd6b650335e11ce0764
Jonathas Costa <jonathas.costa@gmail.com>
2023-06-21 22:07:10 +0100

    Refactoring

--- Cell.js
+++ Cell.js
@@ -1,47 +1,68 @@
 export class Cell {
-    constructor(value = null, annotations = [], onClick = () => {}) {
-      this.value = Array.isArray(value) ? null : value;
-      this.annotations = Array.isArray(value) ? value : annotations;
-      this.type = Array.isArray(value) ? 'Annotations' : 'Value';
-      this.onClick = onClick;
-    }
-  
-    setValue(value) {
-      if (typeof value === 'number') {
-        this.value = value;
-        this.type = 'Value';
-      }
-    }
+  constructor(value = null, annotations = [], onClick = () => { }) {
+    this.value = Array.isArray(value) ? null : value;
+    this.annotations = Array.isArray(value) ? value : annotations;
+    this.type = Array.isArray(value) ? 'Annotations' : 'Value';
+    this.onClick = onClick;
+  }
 
-    toggleAnnotation(number) {
-      const index = this.annotations.indexOf(number);
-      if (index > -1) {
-        this.annotations.splice(index, 1);
-      } else {
-        this.annotations.push(number);
-      }
-      this.type = 'Annotations';
+  setValue(value) {
+    if (typeof value === 'number') {
+      this.value = value;
+      this.type = 'Value';
     }
-  
-    render() {
-      if (this.type === 'Value') {
-        return `<div>${this.value}</div>`;
-      } else {
-        let html = '<table class="annotations">';
-        for (let i = 1; i <= 3; i++) {
-          html += '<tr>';
-          for (let j = 1; j <= 3; j++) {
-            let value = i * 3 + j - 3;
-            if (this.annotations.includes(value)) {
-              html += `<td>${value}</td>`;
-            } else {
-              html += '<td></td>';
-            }
-          }
-          html += '</tr>';
-        }
-        html += '</table>';
-        return html;
-      }
+  }
+
+  removeAnnotation(number) {
+    const index = this.annotations.indexOf(number);
+    if (index > -1) {
+      this.annotations.splice(index, 1);
     }
+  }
+
+  addAnnotation(number) {
+    if (!this.annotations.includes(number)) {
+      this.annotations.push(number);
+    }
+  }
+
+  toggleAnnotation(number) {
+    if (this.annotations.includes(number)) {
+      this.removeAnnotation(number);
+    } else {
+      this.addAnnotation(number);
+    }
+    this.type = 'Annotations';
+  }
+
+  renderValue() {
+    return `<div>${this.value}</div>`;
+  }
+
+  renderAnnotations() {
+    const rows = [1, 2, 3].map(i => this.renderAnnotationsRow(i)).join("");
+    return `<table class="annotations">${rows}</table>`;
+  }
+
+  renderAnnotationsRow(row) {
+    const cells = [1, 2, 3].map(j => this.renderAnnotationsCell(row, j)).join("");
+    return `<tr>${cells}</tr>`;
+  }
+
+  renderAnnotationsCell(row, cell) {
+    let value = row * 3 + cell - 3;
+    if (this.annotations.includes(value)) {
+      return `<td>${value}</td>`;
+    } else {
+      return '<td></td>';
+    }
+  }
+
+  render() {
+    if (this.type === 'Value') {
+      return this.renderValue();
+    } else {
+      return this.renderAnnotations();
+    }
+  }
 }
